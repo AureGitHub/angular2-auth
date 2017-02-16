99248a656cbaeef300110eec92c3a4807e77318e
De Sande Villarroel <jdesande@ttec.es>
2017-02-16 13:40:09 +0100

    con servicio para obtener datos (no funciona)

--- src/app/dashboard/privado/privado.component.ts
+++ src/app/dashboard/privado/privado.component.ts
@@ -1,38 +1,97 @@
-import { Component, ChangeDetectionStrategy, ViewChild } from '@angular/core';
-import { ModalDirective } from 'ng2-bootstrap/modal/modal.component';
+import { Observable } from 'rxjs/Rx';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { GridDataResult } from '@progress/kendo-angular-grid';
+import { State, process } from '@progress/kendo-data-query';
+
+import { Product } from './model';
+import { EditService } from '../../_service/edit.service';
 
 @Component({
-  selector: 'app-privado',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-privado',  
   templateUrl: './privado.component.html',
   styleUrls: ['./privado.component.css']
 })
-export class PrivadoComponent {
+export class PrivadoComponent  implements OnInit {
 
-  constructor() { }
+    public view: Observable<GridDataResult>;
+    public gridState: State = {
+        sort: [],
+        skip: 0,
+        take: 10
+    };
+    public formGroup: FormGroup;
 
- 
-@ViewChild('childModal') public childModal:ModalDirective;
+    private editService: EditService;
+    private editedRowIndex: number;
 
-	// Button
-	public singleModel:string = '1';
-	public radioModel:string = 'Middle';
-	public checkModel:any = {left: false, middle: true, right: false};
+    constructor(@Inject(EditService) editServiceFactory: any) {
+        this.editService = editServiceFactory();
+    }
 
-	public alerts:Array<Object> = [
-	   {
-	     type: 'danger',
-	     msg: 'Oh snap! Change a few things up and try submitting again.'
-	   },
-	   {
-	     type: 'success',
-	     msg: 'Well done! You successfully read this important alert message.',
-	     closable: true
-	   }
-	 ];
+    public ngOnInit(): void {
+        this.view = this.editService.map(data => process(data, this.gridState));
 
-	 public closeAlert(i:number):void {
-	   this.alerts.splice(i, 1);
-	 }
+        this.editService.read();
+    }
+
+    public onStateChange(state: State) {
+        this.gridState = state;
+
+        this.editService.read();
+    }
+
+    protected addHandler({sender}) {
+        this.closeEditor(sender);
+
+        this.formGroup = new FormGroup({
+            'ProductID': new FormControl(),
+            'ProductName': new FormControl("", Validators.required),
+            'UnitPrice': new FormControl(0),
+            'UnitsInStock': new FormControl("", Validators.compose([Validators.required, Validators.pattern('^[0-9]{1,2}')])),
+            'Discontinued': new FormControl(false)
+        });
+
+        sender.addRow(this.formGroup);
+    }
+
+    protected editHandler({sender, rowIndex, dataItem}) {
+        this.closeEditor(sender);
+
+        this.formGroup = new FormGroup({
+            'ProductID': new FormControl(dataItem.ProductID),
+            'ProductName': new FormControl(dataItem.ProductName, Validators.required),
+            'UnitPrice': new FormControl(dataItem.UnitPrice),
+            'UnitsInStock': new FormControl(dataItem.UnitsInStock, Validators.compose([Validators.required, Validators.pattern('^[0-9]{1,2}')])),
+            'Discontinued': new FormControl(dataItem.Discontinued)
+        });
+
+        this.editedRowIndex = rowIndex;
+
+        sender.editRow(rowIndex, this.formGroup);
+    }
+
+    protected cancelHandler({sender, rowIndex}) {
+        this.closeEditor(sender, rowIndex);
+    }
+
+    private closeEditor(grid, rowIndex = this.editedRowIndex) {
+        grid.closeRow(rowIndex);
+        this.editedRowIndex = undefined;
+        this.formGroup = undefined;
+    }
+
+    protected saveHandler({sender, rowIndex, formGroup, isNew}) {
+        const product: Product = formGroup.value;
+
+        this.editService.save(product, isNew);
+
+        sender.closeRow(rowIndex);
+    }
+
+    protected removeHandler({dataItem}) {
+        this.editService.remove(dataItem);
+    }
 
 }
--- src/app/dashboard/privado/privado.module.ts
+++ src/app/dashboard/privado/privado.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PrivadoComponent }     from './privado.component';
 import { CommonModule } from '@angular/common';
+import { EditService } from '../../_service/edit.service';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { Jsonp, JsonpModule } from '@angular/http';
 
 @NgModule({
-    imports: [RouterModule,CommonModule,    
+    imports: [RouterModule,CommonModule,   GridModule,  JsonpModule
     ],
     declarations: [PrivadoComponent],
-    exports: [PrivadoComponent]
+    exports: [PrivadoComponent],
+     providers: [ {
+            deps: [Jsonp],
+            provide: EditService,
+            useFactory: (jsonp: Jsonp) => () => new EditService(jsonp)
+        }],
 })
 
 export class PrivadoModule { }
